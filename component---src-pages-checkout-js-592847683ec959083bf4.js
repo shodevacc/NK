1646dee9ace19e6955ecf7892760d2d176d8ed1e
shoaib alyaan <SHO@shoaibs-MacBook-Pro-2.local>
2021-02-24 22:57:59 +0530

    Updates

---
+++ component---src-pages-checkout-js-592847683ec959083bf4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{UxAK:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),c=n.n(a),u=n("Bl7J"),l=n("2Bqf");t.default=function(){return c.a.createElement(u.a,{title:"checkout"},c.a.createElement(l.a,null))}}}]);
+//# sourceMappingURL=component---src-pages-checkout-js-592847683ec959083bf4.js.map
